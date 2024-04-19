cfce95898df9806701956bae1b70670540484c9d
Walker Milhoan <walker@milhoandesign.com>
2024-04-19 11:38:11 -0600

    Playing with fire

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+/*
 // My shitty Js code for the simple calendar
 const select = document.querySelector("select");
 const headingTwo = document.querySelector("h2");
@@ -10,3 +11,38 @@ function createCalendar () {
     const selectedMonth = select.value; 
     headingTwo.textContent = selectedMonth;
 }; 
+*/ 
+
+const select = document.querySelector("select");
+const list = document.querySelector("ul");
+const headingTwo = document.querySelector("h2");
+
+select.addEventListener("change", () => {
+  const choice = select.value;
+
+  let days = 31;
+  if (choice === "February") {
+    days = 28;
+  } else if (
+    choice === "April" ||
+    choice === "June" ||
+    choice === "September" ||
+    choice === "November"
+  ) {
+    days = 30;
+  }
+
+  createCalendar(days, choice);
+});
+
+function createCalendar(days, choice) {
+  list.innerHTML = "";
+  headingTwo.textContent = choice;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+createCalendar(31, "January");
